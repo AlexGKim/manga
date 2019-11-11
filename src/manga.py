2bc6288e4a51fa523d48504a3ea2b21cf9f3dde9
Alex Kim <agkim@lbl.gov>
2019-11-11 15:13:47 -0800

    add more comments and clean out commented out stuff

--- src/manga.py
+++ src/manga.py
@@ -1,77 +1,70 @@
 #!/usr/bin/env python
 import pystan
 import numpy
-import matplotlib.pyplot as plt
 import pickle
+
+'''
+Given a set of central wavelengths of a Manga spectrum, return a mask of elements that have telluric features.
+
+That this could be made much more efficient but since it isn't run often I don't bother.
+'''
 
 def telluric_mask(lam):
     data = numpy.loadtxt('../data/telluric.txt')
     logicals=[]
+    # I don't know exactly what the edges are of the MANGA spectral bins so I make a guess
     edges = (lam+numpy.roll(lam,1))/2
     edges[0]=lam[0]-(lam[1]-lam[0])/2
     edges = numpy.append(edges, lam[-1]+(lam[-1]-lam[-2])/2)
-    # print(edges[0],edges[1],edges[-1],lam[0],lam[-1],len(edges),len(lam))
-    # wefwe
-    # dlam = lam-numpy.roll(lam,1)
-    # dlam[0]=lam[1]-lam[0]
-    # edges = numpy.array([lam[0]-(lam[1]-lam[0])/2,lam+dlam/2])
-    # print(edges.shape)
-    # wefe 
+
     mask = []
+    # loop over data bins
     for i in range(len(lam)):
         temp=[]
+        # Loop over telluric bins
         for lmin,lmax in zip(data[:,0],data[:,1]):
+            # Do the spectral and telluric bins overlap
             temp.append((edges[i] <= lmin  and lmin<=edges[i+1]) or (lmin<=edges[i+1] and edges[i+1]<=lmax))
         mask.append(numpy.any(temp))
 
-
-    # for a0, a1 in zip(lam_lower,lam_upper):
-    #     temp=[]
-    #     for lmin,lmax in zip(data[0],data[1]):
-    #         temp.append((a0 <= lmin  and lmin<=a1) or (lmin<=a1 and a1<=lmax))
-    #         print(a0,a1,lmin,lmax,temp[-1])
-    #     mask.append(numpy.any(temp))
-    #     print(mask[-1])
     return numpy.logical_not(mask)
 
 if __name__ == "__main__":
 
+    # 
     chains=4
     iteration=2000
     warmup=1000
     n_jobs=4
 
-    #range of (1+v) that are possible
+    # range of ln(1+v) that are possible
+    # This helps STAN out a bit
     arange =1e-3
     amin=10**-arange
     amax=10**arange
 
     # data range observer frame
+    # The data will be cut by this
+    # The model range will be this expanded by the possible velocity range
     lmin=3800
     lmax=10000
 
 
-    # # Model the underlying spectrum with a spectrum with the following wavelengths
-    # # Probably would be better for the model spectrum to be natively in log(lambda)
-    # dlam = 2.
-    # lam_m = numpy.arange(lmin*amin-dlam,lmax*amax+dlam*1.5,dlam)
+    # Model the underlying spectrum with a spectrum with the following wavelengths
+    # Note that the native resolution of MANGA is constant R, so it makes sense to
+    # work with a constant resolution grid
 
     #native 1-pix resolution is 
     native_R = 4342.444731562085
-    R = 2000.
+    R = 2000    # This is somewhat arbitrary, could be increase a little
     lam_m = numpy.arange(numpy.log(lmin)-arange-5/R, numpy.log(lmax)+arange+5.5/R,1/R)
     lam_m = numpy.exp(lam_m)
 
     # Read in data
-
     filenames = ['manga-7991-12701-38-26.txt','manga-7991-12701-38-38.txt']
-
     filedata = []
     for filename in filenames:
         filedata.append(numpy.loadtxt('../data/'+filename))
-
-    # print(filedata[0][:,0][numpy.logical_not(telluric_mask(filedata[0][:,0]))])
-    # wefe
 
     # truncate the wavelength range for the data
     w0 = numpy.logical_and.reduce((filedata[0][:,0]>= lmin,filedata[0][:,0] < lmax,filedata[0][:,3]==1,telluric_mask(filedata[0][:,0])))
@@ -80,33 +73,9 @@ if __name__ == "__main__":
     nlam0= w0.sum()
     nlam1 = w1.sum()
 
-
-    # si = 1
-    # x0=100
-    # fft = numpy.fft.fft(filedata[0][:,1])
-    # fft=fft * 0.5*(numpy.tanh((numpy.arange(len(fft))-x0)/si)+1)
-    # ifft = numpy.fft.ifft(fft)
-    # res1= numpy.absolute(ifft)
-    # fft = numpy.fft.fft(filedata[1][:,1])
-    # fft=fft * 0.5*(numpy.tanh((numpy.arange(len(fft))-x0)/si)+1)
-    # ifft = numpy.fft.ifft(fft)
-    # res2= numpy.absolute(ifft)
-
-    #  # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
-    # plt.plot(filedata[0][:,0],res1[:],label='0 filt')
-
-    # # plt.plot(filedata[1][w1,0],filedata[1][w1,1],label='1')
-    # plt.plot(filedata[1][:,0],res2[:],label='1 filt')
-    # plt.yscale('log')
-    # plt.legend()
-    # plt.show()
-    # wefe
-
-
-    # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
-    # plt.plot(filedata[0][w0,0],numpy.absolute(ifft),label='filt')
-    # plt.show()
-    # wefe
+    # subtract out a third order polynomial to get rid of smooth continuum
+    # I did try low-pass filter with FFT but these leave periodic features
+    # we want to avoid
 
     order =3
     p = numpy.polyfit(numpy.log(filedata[0][w0,0]),filedata[0][w0,1], order)
@@ -121,45 +90,23 @@ if __name__ == "__main__":
     p = numpy.polyfit(numpy.log(filedata[1][w,0]),filedata[1][w,1], order) 
     res2 = filedata[1][w1,1] - numpy.polyval(p,numpy.log(filedata[1][w1,0]))
 
-     # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
-    # plt.plot(filedata[0][w0,0],res1,label='0 filt')
-
-    # plt.plot(filedata[1][w1,0],filedata[1][w1,1],label='1')
-    # plt.plot(filedata[1][w1,0],res2,label='1 filt')
-    # plt.yscale('log')
-    # plt.legend()
-    # plt.show()
-    # wef
-
-    # Model spectrum in log(lambda), STAN runs faster with sum rather than multiplication
-
+    # Do the STAN HMC
+    # Note that the wavelengths are in log space.
     data = {'lam0': numpy.log(filedata[0][w0,0]), 'flux0': res1, 'flux0_var':1/filedata[0][w0,2], 'lam1': numpy.log(filedata[1][w1,0]), 'flux1': res2, 'flux1_var':1/filedata[1][w1,2],
         'lam_m':numpy.log(lam_m), 'nlam_m': len(lam_m),'nlam0':nlam0,'nlam1':nlam1,'arange':arange, 'R': R}
 
     # As an initial condition take the average spectrum
     ones = numpy.interp(numpy.log(lam_m),numpy.log(filedata[0][w0,0]),res1) + numpy.interp(numpy.log(lam_m),numpy.log(filedata[1][w1,0]),res2) 
     ones = ones/2.
-    # ones[ones <=0] = 1e-3
 
     init = [{
         'flux': ones, 'a_scale':0.0, 'n':[0.5,0.5], 'sigma2':0.03**2
     } for i in range(chains)]
 
-    # init = [{
-    #     'flux': ones, 'n':[0.5,0.5], 'sigma':0.03
-    # } for i in range(chains)]
-
-    sm = pystan.StanModel(file='manga_K.stan')
-
+    sm = pystan.StanModel(file='manga.stan')
     fit = sm.sampling(data=data,init=init,chains=chains,n_jobs=n_jobs,iter=iteration,warmup = warmup)
 
-    with open('fit_K.pkl', 'wb') as handle:
+    # Save results
+    with open('fit.pkl', 'wb') as handle:
         pickle.dump(fit.extract(), handle)
     print(fit)
-
-    # plt.plot(data['lam0'],data['flux0'],label='0')
-    # plt.plot(data['lam1'],data['flux1'],label='1')
-    # plt.plot(lam_m,ones,label='avg')
-    # plt.legend()
-    # plt.show()
-    # wef
--- src/manga.stan
+++ src/manga.stan
@@ -1,4 +1,10 @@
 functions{
+
+  // Linear interpolator  THIS IS NOT USED
+
+  // The algorithm assumes that the target wavelengths are in order
+  // and that the model wavelengths are in order
+
   vector lerp(vector x0, vector x1, vector y1){
     vector[dims(x0)[1]] ans;
     int ind1;
@@ -13,47 +19,48 @@ functions{
     return ans;
   }
 
+  // y = Kz
+  // To make STAN faster, I don't do the full matrix multiplication, but rather only include terms
+  // within 5 resolution elements of the target wavelength
+
+  // The algorithm assumes that the target wavelengths are in order
+  // and that the model wavelengths are in order
+
   vector kerp(vector lam0, vector lam_m, vector flux, real R){ 
     vector[dims(lam0)[1]] ans;
-    // matrix[dims (lam0)[1],dims(lam_m)[1]] K_0;
     real diff;
     real R2over2;
     real Rinv;
     int ind;
     int mark;
-    // vector[dims(lam_m)[1]] diff;
+    real range;
 
+    range=5.;
     R2over2=R*R/2;
     Rinv=1./R;
-    mark=1;
+
+    mark=1; // marks the minimum possible model wavelength
+
+    // Loop over all data wavelengths
+
     for (i in 1:dims(lam0)[1]) {
       ans[i]=0;
+
+      // go to the lowest model index that contributes
       ind=mark;
-      // print(lam_m[ind]," ",lam0[i]);
-      while(lam_m[ind+1] < lam0[i]-5*Rinv){
-        // print(lam_m[ind]," ",lam0[i]);
+      while(lam_m[ind+1] < lam0[i]-range*Rinv){
         ind = ind+1;
       }
+      mark=ind; //since the data wavelengths are ordered 
 
-      mark=ind;
-      while(lam_m[ind] < lam0[i]+5*Rinv){
-        // print(lam_m[ind]," ",lam0[i]);
+      // do the matrix multiplication for the model terms that contribute
+      while(lam_m[ind] < lam0[i]+range*Rinv){
         diff = lam_m[ind]-lam0[i];
         diff = diff * diff*R2over2;
         ans[i]=ans[i]+ exp(-diff)*flux[ind];
         ind = ind+1;
       }
-      // print(lam_m[ind]," ",lam0[i]);
-      // diff = lam_m-lam0[i];
-      // diff = diff .* diff*R2over2;
-      // for (j in 1:dims(lam_m)[1]) {
-      //     if (fabs(diff[j]) < 50){
-      //       ans[i]=ans[i]+ exp(-diff[j])*flux[j];
-      //     } 
-      // }
     }
-    // K_0 = exp(-K_0 .* K_0/2.*R2);
-    // return K_0*flux;
     return ans;
   } 
 }
@@ -73,38 +80,21 @@ data {
   vector[nlam1] flux1_var;
 
   vector[nlam_m] lam_m;
-
 }
 
 parameters {
-  vector<lower=-.2, upper=15>[nlam_m] flux;
-  simplex[2] norm;
-  // simplex[2] a_raw;
-  real<lower=-arange,upper=arange> scale_a;
-  real<lower=0> sigma2;
+  vector<lower=-.2, upper=15>[nlam_m] flux; // The "z"'s in the model y = Kz
+  simplex[2] norm;  // The normalization
+  real<lower=-arange,upper=arange> scale_a;  // log-wavelength shift
+  real<lower=0> sigma2; // variance
 }
+
+//  Model for data from spaxel i is
+//  y_i ~ n_i Cauchy (K_i(ln lambda+scale_a_i) z, sigma)
 
 model {
   vector[nlam0] flux0_model;
   vector[nlam1] flux1_model;
-
-  // matrix[nlam0,nlam_m] K_0;
-  // matrix[nlam1,nlam_m] K_1;
-
-  // for (i in 1:nlam0) {
-  //   for (j in 1:nlam_m) {
-  //         K_0[i,j] = -0.5*scale_a+lam0[i]-lam_m[j];
-  //   }
-  // }
-  // K_0 = exp(-K_0 .* K_0/2.*R2);
-   
-  // for (i in 1:nlam1) {
-  //   for (j in 1:nlam_m) {
-  //         K_1[i,j] = 0.5*scale_a+lam1[i]-lam_m[j];
-  //   }
-  // }
-
-  // K_1 = exp(-K_1 .* K_1/2.*R2);
 
   flux0_model = 2*norm[1]*kerp(-0.5*scale_a+lam0,lam_m,flux,R);
   flux1_model = 2*norm[2]*kerp(0.5*scale_a+lam1,lam_m,flux,R);
