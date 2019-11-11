d4a62c7b8896853e8e400d1e1964173ea59674fd
Alex Kim <agkim@lbl.gov>
2019-11-11 10:39:56 -0800

    this works for Satadru. Clean out old code.  Fix sigma2 in the plotting

--- src/manga.py
+++ src/manga.py
@@ -4,11 +4,42 @@ import numpy
 import matplotlib.pyplot as plt
 import pickle
 
+def telluric_mask(lam):
+    data = numpy.loadtxt('../data/telluric.txt')
+    logicals=[]
+    edges = (lam+numpy.roll(lam,1))/2
+    edges[0]=lam[0]-(lam[1]-lam[0])/2
+    edges = numpy.append(edges, lam[-1]+(lam[-1]-lam[-2])/2)
+    # print(edges[0],edges[1],edges[-1],lam[0],lam[-1],len(edges),len(lam))
+    # wefwe
+    # dlam = lam-numpy.roll(lam,1)
+    # dlam[0]=lam[1]-lam[0]
+    # edges = numpy.array([lam[0]-(lam[1]-lam[0])/2,lam+dlam/2])
+    # print(edges.shape)
+    # wefe 
+    mask = []
+    for i in range(len(lam)):
+        temp=[]
+        for lmin,lmax in zip(data[:,0],data[:,1]):
+            temp.append((edges[i] <= lmin  and lmin<=edges[i+1]) or (lmin<=edges[i+1] and edges[i+1]<=lmax))
+        mask.append(numpy.any(temp))
+
+
+    # for a0, a1 in zip(lam_lower,lam_upper):
+    #     temp=[]
+    #     for lmin,lmax in zip(data[0],data[1]):
+    #         temp.append((a0 <= lmin  and lmin<=a1) or (lmin<=a1 and a1<=lmax))
+    #         print(a0,a1,lmin,lmax,temp[-1])
+    #     mask.append(numpy.any(temp))
+    #     print(mask[-1])
+    return numpy.logical_not(mask)
+
 if __name__ == "__main__":
 
     chains=4
-    iteration=300
-    n_jobs=1
+    iteration=2000
+    warmup=1000
+    n_jobs=4
 
     #range of (1+v) that are possible
     arange =1e-3
@@ -28,22 +59,8 @@ if __name__ == "__main__":
     #native 1-pix resolution is 
     native_R = 4342.444731562085
     R = 2000.
-    lam_m = numpy.arange(numpy.log(lmin)-arange, numpy.log(lmax)+arange+1/R/2.,1/R)
+    lam_m = numpy.arange(numpy.log(lmin)-arange-5/R, numpy.log(lmax)+arange+5.5/R,1/R)
     lam_m = numpy.exp(lam_m)
-
-
-    # plot = False
-    # chains=1
-
-    # lmin=3800
-    # lmax=10000
-    # lmin=5000
-    # lmin=7000
-    # dlam = 5.
-
-    # # Model the underlying spectrum with a spectrum with the following wavelengths
-
-    # lam_m = numpy.arange(lmin-5*dlam,lmax+5.5*dlam,dlam)
 
     # Read in data
 
@@ -53,12 +70,43 @@ if __name__ == "__main__":
     for filename in filenames:
         filedata.append(numpy.loadtxt('../data/'+filename))
 
+    # print(filedata[0][:,0][numpy.logical_not(telluric_mask(filedata[0][:,0]))])
+    # wefe
+
     # truncate the wavelength range for the data
-    w0 = numpy.logical_and(filedata[0][:,0]>= lmin,filedata[0][:,0] < lmax)
-    w1 = numpy.logical_and(filedata[1][:,0]>= lmin,filedata[1][:,0] < lmax)
+    w0 = numpy.logical_and.reduce((filedata[0][:,0]>= lmin,filedata[0][:,0] < lmax,filedata[0][:,3]==1,telluric_mask(filedata[0][:,0])))
+    w1 = numpy.logical_and.reduce((filedata[1][:,0]>= lmin,filedata[1][:,0] < lmax,filedata[1][:,3]==1,telluric_mask(filedata[1][:,0])))
 
     nlam0= w0.sum()
     nlam1 = w1.sum()
+
+
+    # si = 1
+    # x0=100
+    # fft = numpy.fft.fft(filedata[0][:,1])
+    # fft=fft * 0.5*(numpy.tanh((numpy.arange(len(fft))-x0)/si)+1)
+    # ifft = numpy.fft.ifft(fft)
+    # res1= numpy.absolute(ifft)
+    # fft = numpy.fft.fft(filedata[1][:,1])
+    # fft=fft * 0.5*(numpy.tanh((numpy.arange(len(fft))-x0)/si)+1)
+    # ifft = numpy.fft.ifft(fft)
+    # res2= numpy.absolute(ifft)
+
+    #  # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
+    # plt.plot(filedata[0][:,0],res1[:],label='0 filt')
+
+    # # plt.plot(filedata[1][w1,0],filedata[1][w1,1],label='1')
+    # plt.plot(filedata[1][:,0],res2[:],label='1 filt')
+    # plt.yscale('log')
+    # plt.legend()
+    # plt.show()
+    # wefe
+
+
+    # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
+    # plt.plot(filedata[0][w0,0],numpy.absolute(ifft),label='filt')
+    # plt.show()
+    # wefe
 
     order =3
     p = numpy.polyfit(numpy.log(filedata[0][w0,0]),filedata[0][w0,1], order)
@@ -73,28 +121,45 @@ if __name__ == "__main__":
     p = numpy.polyfit(numpy.log(filedata[1][w,0]),filedata[1][w,1], order) 
     res2 = filedata[1][w1,1] - numpy.polyval(p,numpy.log(filedata[1][w1,0]))
 
+     # plt.plot(filedata[0][w0,0],filedata[0][w0,1],label='0')
+    # plt.plot(filedata[0][w0,0],res1,label='0 filt')
 
-    # the K matrix usses square exponontial kernal with rho = 1 Angstrom
-    # This is constant for spectrum 0
-    K=numpy.subtract.outer(numpy.log(filedata[0][w0,0]),lam_m)
-    K = numpy.exp(-K**2/2/(1/R)**2)
+    # plt.plot(filedata[1][w1,0],filedata[1][w1,1],label='1')
+    # plt.plot(filedata[1][w1,0],res2,label='1 filt')
+    # plt.yscale('log')
+    # plt.legend()
+    # plt.show()
+    # wef
 
-    data = {'lam0': numpy.log(filedata[0][w0,0],) 'flux0': res1, 'lam1': numpy.log(filedata[1][w1,0]), 'flux1': res2, 'K_0': K,
-        'lam_m':lam_m, 'nlam_m': len(lam_m),'R2':R**2, 'nlam0':nlam0,'nlam1':nlam1}
+    # Model spectrum in log(lambda), STAN runs faster with sum rather than multiplication
 
-    ones = numpy.interp(numpy.log(lam_m),numpy.log(filedata[0][:,0],res1))+ numpy.interp(numpy.log(lam_m),numpy.log(filedata[1][:,0],res2))
-    ones=ones/2
-    # ones[ones <=0] = 1e-6
+    data = {'lam0': numpy.log(filedata[0][w0,0]), 'flux0': res1, 'flux0_var':1/filedata[0][w0,2], 'lam1': numpy.log(filedata[1][w1,0]), 'flux1': res2, 'flux1_var':1/filedata[1][w1,2],
+        'lam_m':numpy.log(lam_m), 'nlam_m': len(lam_m),'nlam0':nlam0,'nlam1':nlam1,'arange':arange, 'R': R}
+
+    # As an initial condition take the average spectrum
+    ones = numpy.interp(numpy.log(lam_m),numpy.log(filedata[0][w0,0]),res1) + numpy.interp(numpy.log(lam_m),numpy.log(filedata[1][w1,0]),res2) 
+    ones = ones/2.
+    # ones[ones <=0] = 1e-3
+
     init = [{
-        'flux': ones, 'a_1': 1, 'n_1':1,'sigma':0.03
+        'flux': ones, 'a_scale':0.0, 'n':[0.5,0.5], 'sigma2':0.03**2
     } for i in range(chains)]
-    if plot:
-        for i in ['0','1']:
-            plt.plot(data['lam'+i],data['flux'+i])
-        plt.show()
 
-    sm = pystan.StanModel(file='manga.stan')
+    # init = [{
+    #     'flux': ones, 'n':[0.5,0.5], 'sigma':0.03
+    # } for i in range(chains)]
 
-    fit = sm.sampling(data=data, iter=300,init=iOOnit,n_jobs=1,chains=chains)
-    pickle.dump( fit, open( "fit.pkl", "wb" ) )
+    sm = pystan.StanModel(file='manga_K.stan')
+
+    fit = sm.sampling(data=data,init=init,chains=chains,n_jobs=n_jobs,iter=iteration,warmup = warmup)
+
+    with open('fit_K.pkl', 'wb') as handle:
+        pickle.dump(fit.extract(), handle)
     print(fit)
+
+    # plt.plot(data['lam0'],data['flux0'],label='0')
+    # plt.plot(data['lam1'],data['flux1'],label='1')
+    # plt.plot(lam_m,ones,label='avg')
+    # plt.legend()
+    # plt.show()
+    # wef
--- src/manga.stan
+++ src/manga.stan
@@ -1,75 +1,116 @@
 functions{
   vector lerp(vector x0, vector x1, vector y1){
-    vector[size(x0)] ans;
+    vector[dims(x0)[1]] ans;
     int ind1;
 
-    ind1=0
-    for (i in 1:size(ans)){
-      while (ind1<size(x0) and x0[i]< x1[ind1+1]) {
-        ind1=ind1+1
+    ind1=1;
+    for (i in 1:dims(ans)[1]){
+      while (x1[ind1+1] < x0[i]) {
+        ind1=ind1+1;
       }
-      ans[i] = 
+      ans[i] = y1[ind1] + (y1[ind1+1]-y1[ind1])/(x1[ind1+1]-x1[ind1])*(x0[i]-x1[ind1]);
     }
-    return sum((v1-mean(v1)) .* (v2-mean(v2)))/num_elements(v1);
+    return ans;
   }
+
+  vector kerp(vector lam0, vector lam_m, vector flux, real R){ 
+    vector[dims(lam0)[1]] ans;
+    // matrix[dims (lam0)[1],dims(lam_m)[1]] K_0;
+    real diff;
+    real R2over2;
+    real Rinv;
+    int ind;
+    int mark;
+    // vector[dims(lam_m)[1]] diff;
+
+    R2over2=R*R/2;
+    Rinv=1./R;
+    mark=1;
+    for (i in 1:dims(lam0)[1]) {
+      ans[i]=0;
+      ind=mark;
+      // print(lam_m[ind]," ",lam0[i]);
+      while(lam_m[ind+1] < lam0[i]-5*Rinv){
+        // print(lam_m[ind]," ",lam0[i]);
+        ind = ind+1;
+      }
+
+      mark=ind;
+      while(lam_m[ind] < lam0[i]+5*Rinv){
+        // print(lam_m[ind]," ",lam0[i]);
+        diff = lam_m[ind]-lam0[i];
+        diff = diff * diff*R2over2;
+        ans[i]=ans[i]+ exp(-diff)*flux[ind];
+        ind = ind+1;
+      }
+      // print(lam_m[ind]," ",lam0[i]);
+      // diff = lam_m-lam0[i];
+      // diff = diff .* diff*R2over2;
+      // for (j in 1:dims(lam_m)[1]) {
+      //     if (fabs(diff[j]) < 50){
+      //       ans[i]=ans[i]+ exp(-diff[j])*flux[j];
+      //     } 
+      // }
+    }
+    // K_0 = exp(-K_0 .* K_0/2.*R2);
+    // return K_0*flux;
+    return ans;
+  } 
 }
 
 data {
   int nlam_m;
-  real R2;
   int nlam0;
   int nlam1;
 
+  real arange;
+  real R;
   vector[nlam0] lam0;
   vector[nlam0] flux0;
+  vector[nlam0] flux0_var;
   vector[nlam1] lam1;
   vector[nlam1] flux1;
-
+  vector[nlam1] flux1_var;
 
   vector[nlam_m] lam_m;
-  matrix[nlam0,nlam_m] K_0;
+
 }
 
 parameters {
-  vector<lower=0.01,upper=100>[nlam_m] flux;
-
-  real<lower=0.95,upper=1.05> a_1;
-  real<lower=0> n_1;
-  real<lower=0> sigma;
+  vector<lower=-.2, upper=15>[nlam_m] flux;
+  simplex[2] norm;
+  // simplex[2] a_raw;
+  real<lower=-arange,upper=arange> scale_a;
+  real<lower=0> sigma2;
 }
-
 
 model {
   vector[nlam0] flux0_model;
   vector[nlam1] flux1_model;
 
-  matrix[nlam1,nlam_m] K_1;
+  // matrix[nlam0,nlam_m] K_0;
+  // matrix[nlam1,nlam_m] K_1;
 
-  for (i in 1:nlam1) {
-    for (j in 1:nlam_m) {
-        // temp = a_1*lam1[i]-lam_m[j];
-        // if (fabs(temp) < 5*dlam){
-          K_1[i,j] = a_1+lam1[i]-lam_m[j];
-        // } else{
-          // K_1[i,j] =0;
-        // }
-    }
-  }
-  K_1 = exp(-K_1 .* K_1/2.*R2);
+  // for (i in 1:nlam0) {
+  //   for (j in 1:nlam_m) {
+  //         K_0[i,j] = -0.5*scale_a+lam0[i]-lam_m[j];
+  //   }
+  // }
+  // K_0 = exp(-K_0 .* K_0/2.*R2);
+   
+  // for (i in 1:nlam1) {
+  //   for (j in 1:nlam_m) {
+  //         K_1[i,j] = 0.5*scale_a+lam1[i]-lam_m[j];
+  //   }
+  // }
 
-  flux0_model = K_0 * flux;
-  // print(K_0);
-  // print(a_1);
-  // print("flux0");
-  // print(flux0_model);
-  // print("flux");
-  // print(flux);
-  flux1_model = n_1*(K_1 * flux);
-  // print("flux1")
-  // print(flux1_model);
+  // K_1 = exp(-K_1 .* K_1/2.*R2);
 
-  target += normal_lpdf(flux0| flux0_model, sigma);
-  target += normal_lpdf(flux1| flux1_model, sigma);
+  flux0_model = 2*norm[1]*kerp(-0.5*scale_a+lam0,lam_m,flux,R);
+  flux1_model = 2*norm[2]*kerp(0.5*scale_a+lam1,lam_m,flux,R);
 
-  target += cauchy_lpdf(sigma|0,0.03);
+  target += cauchy_lpdf(flux0| flux0_model, sqrt(flux0_var+sigma2));
+  target += cauchy_lpdf(flux1| flux1_model, sqrt(flux0_var+sigma2));
+
+  target += cauchy_lpdf(sigma2|0,.0009); // this is based on the dispersion from zero shift
 }
